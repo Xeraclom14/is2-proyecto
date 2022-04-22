0308cd584838d0f48ea829cf676fdf4f206cc035
jonathanavc <jonvenega@gmail.com>
2022-04-22 13:36:01 -0400

    se vienen cositas Signed-off-by: jonathanavc <jonvenega@gmail.com>

--- app.py
+++ app.py
@@ -1,18 +1,31 @@
+from multiprocessing.sharedctypes import Value
 from flask import Flask, render_template, request, redirect, url_for
 from flask_mysqldb import MySQL
 from numpy import insert
 
 app = Flask(__name__)
 
+#encuestador id
+#INSERT INTO `encuestador`(`ap_pat`, `ap_mat`, `prim_nom`,`seg_nom`,`email`) VALUES ('V','C','J','A','1')
+e_id = 1
+
+
+#BD
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = ''
+#Nombre de la BD
 app.config['MYSQL_DB'] = 'encuestas'
 mysql = MySQL(app)
 
+
+#crea un usuario en ala base de datos
 @app.route('/')
 def Index():
-    return "Hola."
+    cur = mysql.connection.cursor()
+    cur.execute("INSERT INTO `encuestador`(`ap_pat`, `ap_mat`, `prim_nom`,`seg_nom`,`email`) VALUES ('V','C','J','A','j@gmail.com')")
+    mysql.connection.commit()
+    return "Encuestador 1 creado"
 
 @app.route('/edit/<id>')
 def edit(id):
@@ -21,11 +34,15 @@ def edit(id):
 @app.route('/create', methods=['GET','POST'])
 def create():
     if request.method == 'POST':
-        cur = mysql.connection.cursor()
-        cur.execute('INSERT INTO encuesta (id_encuestador, titulo) VALUES (1 , %s);', request.form['titulo'])
-        mysql.connection.commit()
-        print(request.form['titulo'])
-        return render_template('creation.html')
+        if(request.form['titulo'] != ""):
+            cur = mysql.connection.cursor()
+            cur.execute("INSERT INTO encuesta (id_encuestador, titulo) VALUES (%s , %s);", (e_id, request.form['titulo']))
+            mysql.connection.commit()
+            print(request.form['titulo'])
+        else:
+            print("Ingerese un valor valido")
+        #redirigir para borrar el formulario de la memoria (no se envia nuevamente si se recarga la pagina)
+        return redirect(url_for("create"))
     else:
         return render_template('creation.html')
 
