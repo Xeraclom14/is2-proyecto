2eae904ba8e0ca06be92e8f82be945e0c18bb10a
jonathanavc <jonvenega@gmail.com>
2022-04-26 00:54:31 -0400

    SI
    Signed-off-by: jonathanavc <jonvenega@gmail.com>

--- app.py
+++ app.py
@@ -21,10 +21,10 @@ mysql = MySQL(app)
 #crea un usuario en ala base de datos
 @app.route('/')
 def Index():
-    cur = mysql.connection.cursor()
-    cur.execute("INSERT INTO `encuestador`(`ap_pat`, `ap_mat`, `prim_nom`,`seg_nom`,`email`) VALUES ('V','C','J','A','j@gmail.com')")
-    mysql.connection.commit()
-    return "Encuestador j@gmail.com creado"
+    #cur = mysql.connection.cursor()
+    #cur.execute("INSERT INTO `encuestador`(`ap_pat`, `ap_mat`, `prim_nom`,`seg_nom`,`email`) VALUES ('V','C','J','A','j@gmail.com')")
+    #mysql.connection.commit()
+    return "INDEX"
 
 @app.route('/edit/<id>')
 def edit(id):
