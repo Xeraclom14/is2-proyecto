8049d3fb3cd0cefdcbeb2fe051f03c8e1ca0db78
jonathanavc <jonvenega@gmail.com>
2022-04-26 19:19:12 -0400

    añadidos los mensajes de confirmación a forms.html
    Signed-off-by: jonathanavc <jonvenega@gmail.com>

--- app.py
+++ app.py
@@ -1,24 +1,28 @@
 from multiprocessing.sharedctypes import Value
-from flask import Flask, render_template, request, redirect, url_for
+from select import select
+from flask import Flask, flash, render_template, request, redirect, url_for, flash
 from flask_mysqldb import MySQL
 
 app = Flask(__name__)
 
-#encuestador id
-#INSERT INTO `encuestador`(`ap_pat`, `ap_mat`, `prim_nom`,`seg_nom`,`email`) VALUES ('V','C','J','A','1')
+###encuestador id
+###INSERT INTO `encuestador`(`ap_pat`, `ap_mat`, `prim_nom`,`seg_nom`,`email`) VALUES ('V','C','J','A','1')
 e_id = 1
 
 
-#BD
+###BD
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = ''
-#Nombre de la BD
+###Nombre de la BD
 app.config['MYSQL_DB'] = 'encuestas'
 mysql = MySQL(app)
 
+###datos de sesión
+app.secret_key = 'mysecretkey'
 
-#crea un usuario en ala base de datos
+
+###crea un usuario en ala base de datos
 @app.route('/')
 def Index():
     #cur = mysql.connection.cursor()
@@ -33,15 +37,18 @@ def edit(id):
 @app.route('/delete/<id>')
 def delete(id):
     cur = mysql.connection.cursor()
+    cur.execute("SELECT * FROM encuesta WHERE id_encuesta = %s;",(id,))
+    datos = cur.fetchall()
     cur.execute("DELETE FROM encuesta WHERE id_encuesta = %s;", (id,))
     mysql.connection.commit()
+    flash("Encuesta \"" + datos[0][2] +"\" eliminada exitosamente.")
     return redirect(url_for("forms"))
 
 @app.route('/create', methods=['GET','POST'])
 def create():
     if request.method == 'POST':
         if(request.form['titulo'] != ""):
-            #TEXTO LIMPIO DE CATEGORIAS
+            ### TEXTO LIMPIO DE CATEGORIAS
             #categorias =  request.form['categoria'].upper().split(',')
             #for categoria in categorias:
             #    categoria = categoria.rstrip()
@@ -51,10 +58,11 @@ def create():
             cur.execute("INSERT INTO encuesta (id_encuestador, titulo) VALUES (%s , %s);", (e_id, request.form['titulo']))
             mysql.connection.commit()
             #print(request.form['titulo'])
+            flash("Encuesta \"" + request.form['titulo'] + "\" ingresada correctamente")
             return redirect(url_for("forms"))
         else:
             print("Ingerese un valor valido")
-        #redirigir para borrar el formulario de la memoria (no se envia nuevamente si se recarga la pagina)
+        ### redirigir para borrar el formulario de la memoria (no se envia nuevamente si se recarga la pagina)
         return redirect(url_for("create"))
     else:
         return render_template('creation.html')
@@ -72,7 +80,7 @@ def forms():
     cur = mysql.connection.cursor()
     cur.execute("SELECT * FROM encuesta")
     datos = cur.fetchall()
-    print(datos)
+    #print(datos)
     return render_template('forms.html', forms = datos)
 
 if __name__ == '__main__':
