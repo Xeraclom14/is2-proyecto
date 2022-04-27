0ec6cca31d0bc3d920d48145fcf61e4b81233ba8
rhoda437 <rosanmartin2018@inf.udec.cl>
2022-04-27 16:00:37 -0400

    login modificado

--- create.py
+++ create.py
@@ -10,7 +10,7 @@ def create():
     if request.method == 'POST':
         if(request.form['titulo'] != ""):
             cur = mysql.connection.cursor()
-            cur.execute("INSERT INTO encuesta (id_encuestador, titulo) VALUES (%s , %s);", (e_id, request.form['titulo'],))
+            cur.execute("INSERT INTO encuesta (id_encuesta, id_encuestador, titulo) VALUES (1, %s , %s);", (e_id, request.form['titulo'],))
             mysql.connection.commit()
 
             ### TEXTO LIMPIO DE CATEGORIAS
--- index.py
+++ index.py
@@ -6,7 +6,7 @@ from __main__ import mysql
 
 @app.route('/')
 def Index():
-    cur = mysql.connection.cursor()
-    cur.execute("INSERT INTO `encuestador`(`ap_pat`, `ap_mat`, `prim_nom`,`seg_nom`,`email`) VALUES ('V','C','J','A','j@gmail.com')")
-    mysql.connection.commit()
+    #cur = mysql.connection.cursor()
+    #cur.execute("INSERT INTO `encuestador`(`ap_pat`, `ap_mat`, `prim_nom`,`seg_nom`,`email`,`password`) VALUES ('V','C','J','A','j@gmail.com','asd')")
+    #mysql.connection.commit()
     return "INDEX"
