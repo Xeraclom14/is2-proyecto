cbdbbb7649627865613d83d5cf894488e308c422
rhoda437 <rosanmartin2018@inf.udec.cl>
2022-04-26 21:59:04 -0400

    asd

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from multiprocessing.sharedctypes import Value
 from select import select
-from flask import Flask, flash, render_template, request, redirect, url_for, flash
+from flask import Flask, flash, render_template, request, redirect, url_for, flash, session
 from flask_mysqldb import MySQL
 
 app = Flask(__name__)
@@ -12,8 +12,12 @@ e_id = 1
 
 ###BD
 app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = ''
+#app.config['MYSQL_USER'] = 'root'
+#app.config['MYSQL_PASSWORD'] = ''
+
+app.config['MYSQL_USER'] = 'rhoda437'
+app.config['MYSQL_PASSWORD'] = 'PassAuxParaProyectoIS2$'
+
 ###Nombre de la BD
 app.config['MYSQL_DB'] = 'encuestas'
 mysql = MySQL(app)
@@ -82,6 +86,42 @@ def forms():
     datos = cur.fetchall()
     #print(datos)
     return render_template('forms.html', forms = datos)
+
+@app.route('/login', methods=['GET', 'POST'])
+def logeo():
+    msg = ''
+    # Check if "username" and "password" POST requests exist (user submitted form)
+    if request.method == 'POST' and 'uname' in request.form and 'psw' in request.form:
+
+        # Create variables for easy access
+        username = request.form['uname']
+        password = request.form['psw']        
+
+        # Check if account exists using MySQL
+        #cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor = mysql.connection.cursor()
+        cursor.execute('SELECT * FROM Encuestador WHERE email = %s AND password = %s', (username, password,))
+
+        # Fetch one record and return result
+        account = cursor.fetchone()
+
+        # If account exists in accounts table in out database
+        if account:
+            # Create session data, we can access this data in other routes
+            session['loggedin'] = True
+            session['id'] = account['id']
+            session['username'] = account['username']
+
+            # Redirigir a pag principal
+            return 'Logged in successfully!'
+
+        else:
+            #Cuenta incorrecta
+            msg = 'Incorrect username/password!'
+
+    return render_template('login.html', msg='')
+
+
 
 if __name__ == '__main__':
     app.run(port = 5000, debug = True)
