bf003408a92785675796f2f346e6751edaffa3a0
rhoda437 <rosanmartin2018@inf.udec.cl>
2022-04-28 11:05:20 -0400

    refinamiento de login

--- login.py
+++ login.py
@@ -8,22 +8,35 @@ from __main__ import mysql
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        #Correo insertado.
-        if(request.form['correocito'] != ""):
+        #Correo y contraseña insertados.
+        if(request.form['correocito'] != "" and request.form['passwordcita'] != ""):
+            
             #conexión
             cur = mysql.connection.cursor() 
+            
             #seleccionar cuenta
-            cur.execute("SELECT prim_nom FROM encuestador WHERE email = %s AND password = %s;", (request.form['correocito'], request.form['passwordcita'],))
+            cur.execute("SELECT prim_nom, password FROM encuestador WHERE"
+            +" email = %s AND password = %s;",
+            (request.form['correocito'], request.form['passwordcita'],))
+            
             #filtrar por una (no deberia ser necesario?)
             account = cur.fetchone()
+            #print(account)
 
+            #Si algún dato es incorrecto, sea nombre o contraseña.
             if account is None:
-                flash("Usuario no encontrado")
+                flash("Datos Incorrectos")
                 return redirect(url_for("login"))
 
+            #No sirve.
+            #if(request.form['passwordcita']!=account[1]):
+            #    flash("Contraseña incorrecta")
 
-            flash("Bienvenido, " + account[0] + "")
+            #Si fue exitoso.
+            flash("Bienvenido, " + account[0])
             return redirect(url_for("forms"))
+        
+        #Si no ingresaste nada.
         else:
             flash("Por favor, rellene los campos")
             return redirect(url_for("login"))
