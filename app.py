005a8490d33c8c143eb724420235ba852da13bb3
rhoda437 <yabukeero@gmail.com>
2022-06-20 16:38:02 -0400

    ahora deberia funcionar en los servidores de la u

--- app.py
+++ app.py
@@ -76,5 +76,5 @@ def page_not_found(e):
     return render_template('404.html'), 404
 
 if __name__ == '__main__':
-    app.run(port = 5000, debug = True)
+    app.run(host = '0.0.0.0', port = 5008, debug = True)
 
